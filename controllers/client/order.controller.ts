9c3bfb64292e23328eb2cc2c73a032895e364941
Nguyễn Đăng Cường <dangcuong201004@gmail.com>
2025-03-02 00:01:35 +0700

    Save, Update database sequenlize

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,14 +1,37 @@
 
 import { Request, Response } from "express";
+import Order from "../../models/order.model";
+import { generateOrderCode } from "../../helpers/generate.helper";
 
 
 // [GET] /order
 export const index = async (req: Request, res: Response) => {
 
-    console.log(req.body)
+    const data = req.body;
+
+    const dataOrder = {
+        code: "",
+        fullName: data.info.fullName,
+        phone: data.info.phone,
+        note: data.info.note,
+        status: "initial"
+    }
+
+    const order = await Order.create(dataOrder)
+    const orderId = order.dataValues.id
+    const code = generateOrderCode(orderId);
+
+    await Order.update({
+        code: code,
+    }, {
+        where: {
+            id: orderId,
+        }
+    })
 
     res.json({
         code: 200,
-        message: "Đặt tour thành công"
+        message: "Đặt tour thành công",
+        orderCode: code,
     })
 }
